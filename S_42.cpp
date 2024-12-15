a9c86ffb46643ebab8f44f4d488f729bb89a64a8
Henserk <dima.konopelko.96@gmail.com>
2024-12-15 18:27:41 +0800

    Update S_42.cpp

--- S_42.cpp
+++ S_42.cpp
@@ -1,20 +1,24 @@
 #include <iostream>
 using namespace std;
 
-int sort(int arr[]){
-    for (int i=0;i<10;i++){
-        for (int j=0;j<10-1-i;j++){
-            if(arr[j]< arr[j+1]){
-            swap(arr[j],arr[j+1]);
+void sort(int arr[]) {  //изменил тип функции sort на void, так как она не возвращает никакого значения.
+    for (int i=0; i<10; i++) {
+        for (int j=0; j<10-1-i; j++) {
+            if(arr[j]<arr[j+1]) {
+//Вместо использования функции swap, создал временную переменную temp
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
             }
         }
     }
-    for (int i=0;i<10;i++){
-        cout<<arr[i]<<" ";
+    for (int i=0; i<10; i++) {
+        cout << arr[i] << " ";
     }
 }
-int main(){
-    int arr[10]={1, 8, 3, 6, 5, 4, 9, 2, 8, 12};
+
+int main() {
+    int arr[10] = {1, 8, 3, 6, 5, 4, 9, 2, 8, 12};
     sort(arr);
     return 0;
 }
